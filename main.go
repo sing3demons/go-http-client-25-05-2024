45125166b4f988a8837d37806e95a609620d0ad4
sing3demons <sing3demons@live.com>
2024-02-26 21:12:44 +0700

    Add timeout to HTTP client and set request headers

--- main.go
+++ main.go
@@ -4,25 +4,37 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
 	url := "https://official-joke-api.appspot.com/random_joke"
-	resp, err := http.Get(url)
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error creating request:", err)
 		return
 	}
-	defer resp.Body.Close()
 
-	// Read and use the response data here
-	fmt.Println("Response status:", resp.Status)
-	fmt.Println("Response headers:", resp.Header)
+	// req.Header.Add("Authorization", "Bearer API_KEY")
+	req.Header.Add("Content-Type", "application/json")
 
-	data, err := io.ReadAll(resp.Body)
+	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error sending request:", err)
 		return
 	}
-	fmt.Println("Response body:", string(data))
+	defer res.Body.Close()
+
+	fmt.Println("Status code:", res.StatusCode)
+	fmt.Println("Status:", res.Status)
+	fmt.Println("Header:", res.Header)
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+	fmt.Println("Body:", string(body))
 }
