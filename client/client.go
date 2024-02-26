75da51538237375293825dba24ef49fb283a02b7
sing3demons <sing3demons@live.com>
2024-02-26 21:26:54 +0700

    Add HTTP client package and refactor main.go

---
+++ client/client.go
@@ -0,0 +1,86 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type HttpClient interface {
+	Get(url string, headers map[string]string) (string, error)
+	Post(url string, payload any, headers map[string]string) (string, error)
+}
+
+type httpClient struct {
+	Client http.Client
+}
+
+func NewHttpClient() HttpClient {
+	timeoutStr := os.Getenv("TIMEOUT")
+	if timeoutStr == "" {
+		timeoutStr = "10"
+	}
+
+	timeout, err := time.ParseDuration(timeoutStr)
+	if err != nil {
+		timeout = 10
+	}
+
+	return &httpClient{
+		Client: http.Client{
+			Timeout: timeout * time.Second,
+		},
+	}
+}
+
+func (client *httpClient) makeRequest(method, url string, payload io.Reader, headers map[string]string) (string, error) {
+	req, err := client.buildRequest(method, url, payload, headers)
+	if err != nil {
+		return "", err
+	}
+
+	response, err := client.Client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), err
+}
+
+func (client *httpClient) buildRequest(method, url string, payload io.Reader, headers map[string]string) (*http.Request, error) {
+
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	return req, nil
+}
+
+func (client *httpClient) Get(url string, headers map[string]string) (string, error) {
+	return client.makeRequest(http.MethodGet, url, nil, headers)
+}
+
+func (client *httpClient) Post(url string, payload any, headers map[string]string) (string, error) {
+
+	payloadData, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	return client.makeRequest(http.MethodPost, url, bytes.NewReader(payloadData), headers)
+}
--- main.go
+++ main.go
@@ -2,39 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"time"
+
+	"github.com/sing3demons/go-http-client/client"
 )
 
 func main() {
 	url := "https://official-joke-api.appspot.com/random_joke"
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	// client := &http.Client{
+	// 	Timeout: 10 * time.Second,
+	// }
+
+	c := client.NewHttpClient()
+	body, err := c.Get(url, nil)
 	if err != nil {
-		fmt.Println("Error creating request:", err)
+		fmt.Println(err)
 		return
 	}
 
-	// req.Header.Add("Authorization", "Bearer API_KEY")
-	req.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer res.Body.Close()
-
-	fmt.Println("Status code:", res.StatusCode)
-	fmt.Println("Status:", res.Status)
-	fmt.Println("Header:", res.Header)
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return
-	}
-	fmt.Println("Body:", string(body))
+	fmt.Println(body)
 }
